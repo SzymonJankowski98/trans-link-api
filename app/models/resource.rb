e9cba8242b9acf77658840c876381af1290b6d5c
Szymon Jankowski <jankowski.szymon.98@gmail.com>
2023-03-19 08:15:54 +0100

    Add annotate gem

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: resources
+#
+#  id         :bigint           not null, primary key
+#  type       :string
+#  visibility :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_resources_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Resource < ApplicationRecord
   VISIBILITIES = [
     PUBLIC = "public",
